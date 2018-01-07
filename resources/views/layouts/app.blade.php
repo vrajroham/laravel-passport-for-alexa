a067875eda8e0b548eb04d771dd6614d5c661a18
Vaibhavraj Roham <vaibhavraj.developer@gmail.com>
2018-01-07 22:59:01 +0530

    OAuth dashboard added to app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,7 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href="{{ route('oauth-dashboard') }}">OAuth Dashboard</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/oauth-dashboard', 'HomeController@oAuthDashboard')->name('oauth-dashboard');
